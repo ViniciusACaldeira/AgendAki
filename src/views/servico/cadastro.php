37d17cb0aedc36c158b8a210daee1a71a6be9a47
ViniciusACaldeira <vinicius.caldeira.araujo@gmail.com>
2025-08-25 08:40:49 -0300

    Nova tela de cadastro para serviço

--- src/views/servico/cadastro.php
+++ src/views/servico/cadastro.php
@@ -1,23 +1,36 @@
-<?php
-    use Vennizlab\Agendaki\helpers\Flash;
-    Flash::print( ); 
-?>
+<link rel="stylesheet" href="/assets/styles/cadastro.css">
+<link rel="stylesheet" href="/assets/styles/toast.css">
+
+<div id="toast-container"></div>
 
 <a href="/servico">Voltar</a>
 
-<form action="/servico/cadastrar" method="POST">
-    <label for="nome">Nome:</label>
-    <input type="text" id="nome" name="nome" require >
-    <br>
-    <label for="descricao]">Descrição:</label>
-    <textarea name="descricao" id="descricao"></textarea>
-    <br>
-    <label for="preco">Preço</label>
-    <input type="number" id="preco" name="preco" min="0.00" step="0.01">
+<section>
+    <h1>Cadastro</h1>
 
-    <button type="submit">Cadastrar</button>
-</form>
+    <form id="form_cadastro_servico" method="POST">
+        <div class="field">
+            <label for="nome">Nome:</label>
+            <input type="text" id="nome" name="nome" require >
+        </div>
+        
+        <div class="field">
+            <label for="descricao">Descrição:</label>
+            <textarea name="descricao" id="descricao"></textarea>
+        </div>
+        
+        <div class="field">
+            <label for="preco">Preço</label>
+            <input type="number" id="preco" name="preco" min="0.00" step="0.01">
+        </div>
+        
+        <button type="submit">Cadastrar</button>
+    </form>
+</section>
 
+<script src="/assets/script/toast.js"></script>
+<script src="/assets/script/util.js"></script>
+<script src="/assets/script/modal.js"></script>
 <script>
     document.querySelector('form').addEventListener( 'submit', async (event) =>{
         event.preventDefault( );
@@ -30,10 +43,26 @@
         })
         .then( response => response.json() )
         .then( data => {
-            console.log( data );
-            console.log( data['data'] );
+            const status = data['status'];
+            const erros = data['data']['erros'];
+            
+            if( erros != undefined )
+                erros.forEach( (e) => { mostrarToast( e, TOAST_ERRO ) } );
+            else
+                mostrarModal( );
         })
         .catch( error => console.log( error ) );
     });
 
+    function limpaForm( )
+    {
+        document.querySelector( "#nome" ).value = "";
+        document.querySelector( "#descricao" ).value = "";
+        document.querySelector( "#preco").value = "";
+    }
+
+    function mostrarModal( )
+    {
+        modal_abrir( { titulo: "Cadastrar outro?", botoes: [ {texto: "Voltar a tela de início", acao: (modal) => { redireciona( "/servico" ) } }, {texto: "Cadastrar outro", acao: (modal) => { limpaForm(); modal_fechar( ) }}]} )
+    }
 </script>
